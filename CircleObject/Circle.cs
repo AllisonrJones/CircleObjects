36c729f8de920d430eae5fdc8f85824b8397c882
allison <allisonrjones@gmail.com>
2023-01-23 23:41:24 -0500

    intial

---
+++ CircleObject/Circle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace CircleObject
+{
+	public class Circle
+	{
+
+		double area;
+		double circumference;
+		double formattedArea;
+		double formattedCircumference;
+
+		public double radius { get; set; }
+
+		public Circle(double Radius)
+		{
+			this.radius = Radius;
+	
+        }
+		private double FormatNumber(double Radius)
+		{
+
+			return Math.Round(Radius, 2);
+            
+		}
+		public double CalculateCircumference(double Radius)
+		{
+			return circumference = 2 * (Math.PI * Radius);
+		}
+		public double CalculateFormattedCircumference(double Radius, Int32 y)
+		{
+			return  Math.Round(Radius, 2);
+		}
+		public double CalculateArea(double Radius)
+		{
+			return area=  Math.PI * Math.Pow(Radius, 2);
+
+		
+		}
+		public double CalculateFormattedArea(double Radius, Int32 y)
+		{
+			return  Math.Round(Radius, 2);
+		}
+		
+	}
+}
+
+//Console.WriteLine($"The radius is {radius}");
+//			Console.WriteLine();
+//			Console.WriteLine($"Circumference: {CalculateFormattedCircumference}");
+//			Console.WriteLine();
+//			Console.WriteLine($"Area: {CalculateFormattedArea}");
+//			Console.Clear();
---
+++ CircleObject/Program.cs
@@ -0,0 +1,32 @@
+﻿using CircleObject;
+
+Console.WriteLine("Welcome to the Circle Tester");
+bool runProgram = true;
+while (true)
+{
+    Console.WriteLine("Please enter a radius");
+    double radius = double.Parse(Console.ReadLine());
+
+    List<Circle> Objects = new List<Circle>()
+{
+    new Circle(radius)
+};
+
+    foreach (Circle size in Objects)
+    {
+        Console.WriteLine($"{size.radius} {size.CalculateArea(radius)} {size.CalculateCircumference(radius)}");
+    }
+    Console.WriteLine("Would you like to continue? y/n?");
+    string response = Console.ReadLine().ToLower().Trim();
+
+    if (response == "y")
+    {
+        runProgram = true;
+    }
+    else if (response == "n")
+    {
+        runProgram = false;
+        Console.WriteLine($"GoodBye! You made {Objects.Count} circles");
+        
+    }
+}
